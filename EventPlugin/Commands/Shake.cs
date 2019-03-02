78ec970f8408dd407c6e2c990ba1395a7b0690e0
All The Time Gaming <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-02 16:06:40 -0600

    Shake Commands

---
+++ EventPlugin/Commands/Shake.cs
@@ -0,0 +1,63 @@
+﻿using Smod2.Commands;
+using Smod2;
+using Smod2.API;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using ItemManager;
+using ItemManager.Events;
+using RemoteAdmin;
+using scp4aiur;
+namespace ATTG3
+{
+    class Shake : ICommandHandler
+    {
+        private readonly ATTG3Plugin plugin;
+		private object player2;
+		public Player myPlayer;
+		private bool running;
+
+		Server Server => PluginManager.Manager.Server;
+        IConfigFile Config => ConfigManager.Manager.Config;
+
+		public Scp939PlayerScript PlayerScript { get; private set; }
+
+		public Shake(ATTG3Plugin plugin) => this.plugin = plugin;
+        public string GetCommandDescription() => "Shakes Map";
+        public string GetUsage() => "AGSHAKE";
+
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            if (!(sender is Server) &&
+                sender is Player player &&
+                !plugin.AdminRanks.Contains(player.GetRankName()))
+            {
+                return new[]
+                {
+                    $"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
+                };
+            }
+			running = !running;
+			if (running)
+			{
+				Timing.Run(TimingDelay(0.1f));
+			}
+			return new[]
+			{
+				$"Map Shake is now {(running ? "on" : "off")}."
+			};
+
+
+		}
+		private IEnumerable<float> TimingDelay(float time)
+		{
+			while (running)
+			{
+				plugin.Server.Map.Shake();
+
+				yield return 3f;
+			}
+		}
+	}
+}
