4fcc0117aa279d90262af36486a33c986a64afeb
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:09:47 -0500

    Update O49.cs

--- ATTG4/Items/O49.cs
+++ ATTG4/Items/O49.cs
@@ -1,42 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2;
+﻿using ItemManager;
 using ServerMod2.API;
-using ItemManager;
+using Smod2;
 using Smod2.API;
 using UnityEngine;
 
 namespace ATTG4
 {
-	
-	public class O49 : CustomItem
-	{
+
+    public class O49 : CustomItem
+    {
         private readonly ATTG4Plugin plugin;
 
         public static SmodPlayer SmodPlayer { get; private set; }
         public override bool OnPickup()
-		{
-                PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
-                SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
-                smodPlayer.ChangeRole(Smod2.API.Role.SCP_049, false, false);
-                smodPlayer.SetHealth(500);
+        {
+            PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
+            SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
+            smodPlayer.ChangeRole(Smod2.API.Role.SCP_049, false, false);
+            smodPlayer.SetHealth(500);
 
-                SmodPlayer = new SmodPlayer(PlayerObject);
-                SmodPlayer.PersonalBroadcast(10, "Item is not actavated", false);
+            SmodPlayer=new SmodPlayer(PlayerObject);
+            SmodPlayer.PersonalBroadcast(10, "Item is not actavated", false);
 
-			return true;
-		}
+            return true;
+        }
 
-		public override bool OnDeathDrop(GameObject attacker, DamageType damage)
-		{
+        public override bool OnDeathDrop(GameObject attacker, DamageType damage)
+        {
 
-			return false;
-		}
+            return false;
+        }
 
 
-	}
+    }
 }
 
