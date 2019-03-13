9777ba1d8ce6cf9eb74127e4b3ee80a24f174c9b
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:02:51 -0500

    Update PublicVoteYes.cs

--- EventPlugin/Vote Stuff/PublicVoteYes.cs
+++ EventPlugin/Vote Stuff/PublicVoteYes.cs
@@ -1,39 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2.API;
+﻿using Smod2.EventHandlers;
 using Smod2.Events;
-using Smod2.EventHandlers;
-using UnityEngine;
-
 
 namespace ATTG3
 {
     class Yes : IEventHandlerCallCommand
     {
         private readonly ATTG3Plugin plugin;
-
-        public Yes(ATTG3Plugin plugin) => this.plugin = plugin;
-
+        public Yes(ATTG3Plugin plugin) => this.plugin=plugin;
         public void OnCallCommand(PlayerCallCommandEvent ev)
         {
             string command = ev.Command.ToLower();
-			if (command.StartsWith("yes"))
-			{
-				if (plugin.Voteopen)
-				{
-					plugin.Yes++;
-					ev.ReturnMessage = "Vote Submitted";
-
-				}
-				else
-				{
-					ev.ReturnMessage = "Voteing is not open";
-				}
-				return;
-			}
-		}
+            if (command.StartsWith("yes"))
+            {
+                if (plugin.Voteopen)
+                {
+                    plugin.Yes++;
+                    ev.ReturnMessage="Vote Submitted";
+                }
+                else
+                {
+                    ev.ReturnMessage="Voteing is not open";
+                }
+                return;
+            }
+        }
     }
 }
