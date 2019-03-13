8162e035749d9ad64c0664d66d8a4e84766bb9e8
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:10:42 -0500

    Update RECALL.cs

--- ATTG4/Items/RECALL.cs
+++ ATTG4/Items/RECALL.cs
@@ -1,37 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2;
-using ServerMod2.API;
-using ItemManager;
-using Smod2.API;
-using UnityEngine;
-using System.Threading;
+﻿using ItemManager;
 using scp4aiur;
-using System.Collections;
+using ServerMod2.API;
+using Smod2.API;
+using System.Collections.Generic;
+using UnityEngine;
 
 
 namespace ATTG4
 {
     public class RECALL : CustomItem
     {
-        //List<int>;
-        //private readonly ATTG3Plugin plugin;
-
         readonly float delay = 60;
         private readonly ATTG4Plugin plugin;
 
         public static SmodPlayer SmodPlayer { get; private set; }
         public override bool OnPickup()
         {
-
-                SmodPlayer = new SmodPlayer(PlayerObject);
-                SmodPlayer.PersonalBroadcast(10, "RECALLED ACTIVATING IN 1 MIN ", false);
-                Timing.Run(TimingDelay(delay));
-			return true;
-		}
+            SmodPlayer=new SmodPlayer(PlayerObject);
+            SmodPlayer.PersonalBroadcast(10, "RECALLED ACTIVATING IN 1 MIN ", false);
+            Timing.Run(TimingDelay(delay));
+            return true;
+        }
         public override bool OnDeathDrop(GameObject attacker, DamageType damage)
         {
             return false;
@@ -40,14 +29,11 @@ namespace ATTG4
         {
             float delay = 60;
             int HP = SmodPlayer.GetHealth();
-            
             Vector pos = SmodPlayer.GetPosition();
-
             yield return delay;
             SmodPlayer.SetHealth(HP);
             SmodPlayer.Teleport((pos), true);
-			SmodPlayer.PersonalBroadcast(10, "RECALLED ACTIVATED ", false);
-
-		}
+            SmodPlayer.PersonalBroadcast(10, "RECALLED ACTIVATED ", false);
+        }
     }
 }
