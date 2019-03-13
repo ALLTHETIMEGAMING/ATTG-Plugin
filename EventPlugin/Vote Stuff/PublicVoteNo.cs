e9084b547962e8cab8c70d67ab51bb19289562c7
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:02:30 -0500

    Update PublicVoteNo.cs

--- EventPlugin/Vote Stuff/PublicVoteNo.cs
+++ EventPlugin/Vote Stuff/PublicVoteNo.cs
@@ -1,39 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2.API;
+﻿using Smod2.EventHandlers;
 using Smod2.Events;
-using Smod2.EventHandlers;
 
 namespace ATTG3
 {
     class No : IEventHandlerCallCommand
     {
         private readonly ATTG3Plugin plugin;
-
-        public No(ATTG3Plugin plugin) => this.plugin = plugin;
-
+        public No(ATTG3Plugin plugin) => this.plugin=plugin;
         public void OnCallCommand(PlayerCallCommandEvent ev)
         {
             string command = ev.Command.ToLower();
-            if (command.StartsWith("no")) {
-				if (plugin.Voteopen)
-				{
-					plugin.No++;
-					ev.ReturnMessage = "Vote Submitted";
-
-				}
-				else
-				{
-					ev.ReturnMessage = "Voteing is not open";
-				}
-				return;
+            if (command.StartsWith("no"))
+            {
+                if (plugin.Voteopen)
+                {
+                    plugin.No++;
+                    ev.ReturnMessage="Vote Submitted";
+                }
+                else
+                {
+                    ev.ReturnMessage="Voteing is not open";
+                }
+                return;
             }
-            
-
-            
         }
     }
 }
