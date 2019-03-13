095118fee8f1686c0646f26337d1ad80779c4105
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:08:55 -0500

    Update COM.cs

--- ATTG4/Items/COM.cs
+++ ATTG4/Items/COM.cs
@@ -1,38 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2;
+﻿using ItemManager;
 using ServerMod2.API;
-using ItemManager;
+using Smod2;
 using Smod2.API;
 using UnityEngine;
 
 namespace ATTG4
 {
-
-	public class COM : CustomItem
-	{
+    public class COM : CustomItem
+    {
         private readonly ATTG4Plugin plugin;
-
         public static SmodPlayer SmodPlayer { get; private set; }
         public override bool OnPickup()
-		{
-
-                PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
-                SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
-                smodPlayer.ChangeRole(Smod2.API.Role.SCP_079, false, false);
-                smodPlayer.SetHealth(100000);
-			return true;
-		}
-
-		public override bool OnDeathDrop(GameObject attacker, DamageType damage)
-		{
-
-			return false;
-		}
-
-
-	}
+        {
+            PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
+            SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
+            smodPlayer.ChangeRole(Smod2.API.Role.SCP_079, false, false);
+            smodPlayer.SetHealth(100000);
+            return true;
+        }
+        public override bool OnDeathDrop(GameObject attacker, DamageType damage)
+        {
+            return false;
+        }
+    }
 }
