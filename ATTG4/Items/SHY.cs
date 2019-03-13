e5b54a036153540cf71e86c4ccc1837af29486b8
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:10:58 -0500

    Update SHY.cs

--- ATTG4/Items/SHY.cs
+++ ATTG4/Items/SHY.cs
@@ -1,37 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2;
+﻿using ItemManager;
 using ServerMod2.API;
-using ItemManager;
+using Smod2;
 using Smod2.API;
 using UnityEngine;
 
 namespace ATTG4
 {
 
-	public class SHY : CustomItem
-	{
+    public class SHY : CustomItem
+    {
         private readonly ATTG4Plugin plugin;
 
         public static SmodPlayer SmodPlayer { get; private set; }
         public override bool OnPickup()
-		{
-                PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
-                SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
-                smodPlayer.ChangeRole(Smod2.API.Role.SCP_096, false, false);
-                smodPlayer.SetHealth(500);
-			return true;
-		}
+        {
+            PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
+            SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
+            smodPlayer.ChangeRole(Smod2.API.Role.SCP_096, false, false);
+            smodPlayer.SetHealth(500);
+            return true;
+        }
 
-		public override bool OnDeathDrop(GameObject attacker, DamageType damage)
-		{
+        public override bool OnDeathDrop(GameObject attacker, DamageType damage)
+        {
 
-			return false;
-		}
+            return false;
+        }
 
 
-	}
+    }
 }
