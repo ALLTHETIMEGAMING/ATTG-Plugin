cd900cd4637ac4243153bfdf5870dace179b7471
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:05:35 -0500

    Update Votehand.cs

--- EventPlugin/Vote Stuff/Votehand.cs
+++ EventPlugin/Vote Stuff/Votehand.cs
@@ -1,45 +1,39 @@
-﻿using Smod2;
-using Smod2.API;
+﻿using Smod2.EventHandlers;
 using Smod2.Events;
-using Smod2.EventHandlers;
-using System.Collections.Generic;
-using System.Linq;
-using System;
-using UnityEngine;
 
 namespace ATTG3
 {
-	class CommandEvent : IEventHandlerCallCommand
-	{
-		private readonly ATTG3Plugin plugin;
+    class CommandEvent : IEventHandlerCallCommand
+    {
+        private readonly ATTG3Plugin plugin;
 
-		public CommandEvent(ATTG3Plugin plugin)
-		{
-			this.plugin = plugin;
-		}
+        public CommandEvent(ATTG3Plugin plugin)
+        {
+            this.plugin=plugin;
+        }
 
-		public void OnCallCommand(PlayerCallCommandEvent ev)
-		{
-			string command = ev.Command.ToLower();
-			if (!command.StartsWith("no") && !command.StartsWith("yes")) return;
-			if (plugin.Voteopen != true)
-			{
-				ev.ReturnMessage = "Voteing is not open";
-				return;
-			}
-			string[] s = command.Split(' ');
-			if (s[0] == "no")
-			{
-					plugin.No++;
-					ev.ReturnMessage = "Vote Submitted";
-				return;
-			}
-			else if (s[0] == "yes")
-			{
-					plugin.Yes++;
-				ev.ReturnMessage = "Vote Submitted";
-				return;
-			}
-		}
-	}
+        public void OnCallCommand(PlayerCallCommandEvent ev)
+        {
+            string command = ev.Command.ToLower();
+            if (!command.StartsWith("no")&&!command.StartsWith("yes")) return;
+            if (plugin.Voteopen!=true)
+            {
+                ev.ReturnMessage="Voteing is not open";
+                return;
+            }
+            string[] s = command.Split(' ');
+            if (s[0]=="no")
+            {
+                plugin.No++;
+                ev.ReturnMessage="Vote Submitted";
+                return;
+            }
+            else if (s[0]=="yes")
+            {
+                plugin.Yes++;
+                ev.ReturnMessage="Vote Submitted";
+                return;
+            }
+        }
+    }
 }
