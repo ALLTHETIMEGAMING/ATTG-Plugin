6a729c24d94b6cc86236b0a5f74f7f9edea74726
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 14:56:47 -0500

    Update Ammo.cs

--- EventPlugin/Give/Ammo.cs
+++ EventPlugin/Give/Ammo.cs
@@ -1,8 +1,7 @@
-﻿using Smod2.Commands;
-using Smod2;
+﻿using Smod2;
 using Smod2.API;
+using Smod2.Commands;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace ATTG3
 {
@@ -11,15 +10,13 @@ namespace ATTG3
         private readonly ATTG3Plugin plugin;
         Server Server => PluginManager.Manager.Server;
         IConfigFile Config => ConfigManager.Manager.Config;
-
-        public Ammo(ATTG3Plugin plugin) => this.plugin = plugin;
+        public Ammo(ATTG3Plugin plugin) => this.plugin=plugin;
         public string GetCommandDescription() => "";
         public string GetUsage() => "";
-
         public string[] OnCall(ICommandSender sender, string[] args)
         {
-            if (!(sender is Server) &&
-                sender is Player player &&
+            if (!(sender is Server)&&
+                sender is Player player&&
                 !plugin.AdminRanks.Contains(player.GetRankName()))
             {
                 return new[]
@@ -27,38 +24,31 @@ namespace ATTG3
                     $"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
                 };
             }
-
-            if (Server.GetPlayers().Count < 1)
+            if (Server.GetPlayers().Count<1)
                 return new string[] { "The server is empty!" };
-
             Player caller = (sender is Player send) ? send : null;
-
-
-            if (args.Length > 0)
+            if (args.Length>0)
             {
-                if (args[0].ToLower() == "all" || args[0].StartsWith("*"))
+                if (args[0].ToLower()=="all"||args[0].StartsWith("*"))
                 {
-
                     foreach (Player p in Server.GetPlayers())
                     {
                         p.SetAmmo(AmmoType.DROPPED_5, 100000);
                         p.SetAmmo(AmmoType.DROPPED_7, 100000);
                         p.SetAmmo(AmmoType.DROPPED_9, 100000);
-
                     }
-
                 }
                 Player myPlayer = GetPlayerFromString.GetPlayer(args[0]);
-                if (myPlayer == null) { return new string[] { "Couldn't get player: " + args[0] }; }
-                if (myPlayer.TeamRole.Role != Role.SPECTATOR)
+                if (myPlayer==null) { return new string[] { "Couldn't get player: "+args[0] }; }
+                if (myPlayer.TeamRole.Role!=Role.SPECTATOR)
                 {
                     myPlayer.SetAmmo(AmmoType.DROPPED_5, 100000);
                     myPlayer.SetAmmo(AmmoType.DROPPED_7, 100000);
                     myPlayer.SetAmmo(AmmoType.DROPPED_9, 100000);
-                    return new string[] { myPlayer.Name + " has been given ammo!" };
+                    return new string[] { myPlayer.Name+" has been given ammo!" };
                 }
                 else
-                    return new string[] { myPlayer.Name + " is dead!" };
+                    return new string[] { myPlayer.Name+" is dead!" };
             }
             else
             {
