402ed78473583da6fb06cf29bfe433ca7eacc079
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:01:51 -0500

    Update ST079.cs

--- EventPlugin/SCP COMMANDS/ST079.cs
+++ EventPlugin/SCP COMMANDS/ST079.cs
@@ -1,52 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Smod2.API;
 using Smod2.Commands;
-using Smod2.API;
-using scp4aiur;
-using Smod2;
-using ServerMod2.API;
-using ItemManager;
-using UnityEngine;
-using System.Threading;
-using System.Collections;
+using System.Linq;
 namespace ATTG3
 {
-	class ST079 : ICommandHandler
-	{
-		private readonly ATTG3Plugin plugin;
-		private bool running;
-		public ST079(ATTG3Plugin plugin)
-		{
-			//Constructor passing plugin refrence to this class
-			this.plugin = plugin;
-		}
-
-		public string GetCommandDescription()
-		{
-			// This prints when someone types HELP HELLO
-			return "Command Disabled";
-		}
-
-		public string GetUsage()
-		{
-			// This prints when someone types HELP HELLO
-			return "Command Disabled";
-		}
-
-		public string[] OnCall(ICommandSender sender, string[] args)
-		{
-			if (!(sender is Server) &&
-				sender is Player player &&
-				!plugin.SCPrank.Contains(player.GetRankName()))
-			{
-				return new[]
-				{
-					$"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
-				};
-			}
+    class ST079 : ICommandHandler
+    {
+        private readonly ATTG3Plugin plugin;
+        private bool running;
+        public ST079(ATTG3Plugin plugin)
+        {
+            //Constructor passing plugin refrence to this class
+            this.plugin=plugin;
+        }
+        public string GetCommandDescription()
+        {
+            // This prints when someone types HELP HELLO
+            return "Command Disabled";
+        }
+        public string GetUsage()
+        {
+            // This prints when someone types HELP HELLO
+            return "Command Disabled";
+        }
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            if (!(sender is Server)&&
+                sender is Player player&&
+                !plugin.SCPrank.Contains(player.GetRankName()))
+            {
+                return new[]
+                {
+                    $"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
+                };
+            }
 
             /* = !running;
 			foreach (Smod2.API.Generator Generator in Smod2.PluginManager.Manager.Server.Map.GetGenerators())
@@ -56,12 +42,12 @@ namespace ATTG3
 
 			}*/
             return new[]
-			{
-				$"COMMAND DISABLED"
-			};
+            {
+                $"COMMAND DISABLED"
+            };
 
-		}
+        }
 
 
-	}
+    }
 }
