84e2cbfe6bfd432464ae9e5bb3aad0e9fa3763d8
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:08:33 -0500

    Update LAR.cs

--- ATTG4/Items/LAR.cs
+++ ATTG4/Items/LAR.cs
@@ -1,40 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2;
+﻿using ItemManager;
 using ServerMod2.API;
-using ItemManager;
+using Smod2;
 using Smod2.API;
 using UnityEngine;
 
 namespace ATTG4
 {
-
-	public class LAR : CustomItem
+    public class LAR : CustomItem
     {
         private readonly ATTG4Plugin plugin;
-
         public static SmodPlayer SmodPlayer { get; private set; }
         public override bool OnPickup()
-		{
-
-                PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
-                SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
-                smodPlayer.ChangeRole(Smod2.API.Role.SCP_106, false, false);
-                smodPlayer.SetHealth(500);
-
-			return true;
-		}
-
-		public override bool OnDeathDrop(GameObject attacker, DamageType damage)
-		{
-
-			return false;
-		}
-
-	}
+        {
+            PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
+            SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
+            smodPlayer.ChangeRole(Smod2.API.Role.SCP_106, false, false);
+            smodPlayer.SetHealth(500);
+            return true;
+        }
+        public override bool OnDeathDrop(GameObject attacker, DamageType damage)
+        {
+            return false;
+        }
+    }
 }
 
 
