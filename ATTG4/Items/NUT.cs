5995a0c282b30871d065a7f8f871741b1476b4e9
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 15:09:25 -0500

    Update NUT.cs

--- ATTG4/Items/NUT.cs
+++ ATTG4/Items/NUT.cs
@@ -1,37 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2;
+﻿using ItemManager;
 using ServerMod2.API;
-using ItemManager;
+using Smod2;
 using Smod2.API;
 using UnityEngine;
 
 
 namespace ATTG4
 {
-	public class NUT : CustomItem
-	{
+    public class NUT : CustomItem
+    {
         private readonly ATTG4Plugin plugin;
 
         public static SmodPlayer SmodPlayer { get; private set; }
         public override bool OnPickup()
-		{
-                PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
-                SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
-                smodPlayer.ChangeRole(Smod2.API.Role.SCP_173, false, false);
-                smodPlayer.SetHealth(500);
+        {
+            PluginManager.Manager.Server.Map.Broadcast(10, "A SCP HAS SPAWNED", false);
+            SmodPlayer smodPlayer = new SmodPlayer(PlayerObject);
+            smodPlayer.ChangeRole(Smod2.API.Role.SCP_173, false, false);
+            smodPlayer.SetHealth(500);
             return true;
-		}
+        }
 
-		public override bool OnDeathDrop(GameObject attacker, DamageType damage)
-		{
+        public override bool OnDeathDrop(GameObject attacker, DamageType damage)
+        {
 
-			return false;
-		}
+            return false;
+        }
 
 
-	}
+    }
 }
