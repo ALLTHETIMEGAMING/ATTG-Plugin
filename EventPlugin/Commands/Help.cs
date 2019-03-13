fc13dd9c1bf78de2ea862408fc67c4cb90a5197a
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-03-13 14:54:09 -0500

    Update Help.cs

--- EventPlugin/Commands/Help.cs
+++ EventPlugin/Commands/Help.cs
@@ -1,45 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Smod2.Commands;
-using Smod2.API;
-using scp4aiur;
-using Smod2;
-using ServerMod2.API;
-using ItemManager;
-using UnityEngine;
-using System.Threading;
-using System.Collections;
+﻿using Smod2.Commands;
 namespace ATTG3
 {
-	class Help : ICommandHandler
-	{
-		private readonly ATTG3Plugin plugin;
-		public Help(ATTG3Plugin plugin)
-		{
-			//Constructor passing plugin refrence to this class
-			this.plugin = plugin;
-		}
+    class Help : ICommandHandler
+    {
+        private readonly ATTG3Plugin plugin;
+        public Help(ATTG3Plugin plugin)
+        {
+            //Constructor passing plugin refrence to this class
+            this.plugin=plugin;
+        }
 
-		public string GetCommandDescription()
-		{
-			// This prints when someone types HELP HELLO
-			return "Prints Help command";
-		}
+        public string GetCommandDescription()
+        {
+            // This prints when someone types HELP HELLO
+            return "Prints Help command";
+        }
 
-		public string GetUsage()
-		{
-			// This prints when someone types HELP HELLO
-			return "AGHELP";
-		}
+        public string GetUsage()
+        {
+            // This prints when someone types HELP HELLO
+            return "AGHELP";
+        }
 
-		public string[] OnCall(ICommandSender sender, string[] args)
-		{
-			return new[]
-			{
-                "EVENT PLUGIN COMMANDS",
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            return new[]
+            {
+                "ATTG COMMAND PLUGIN COMMANDS",
                 "-------------MAP COMMANDS-------------",
                 "(AGTL) Turns on all Tleslas",
                 "(AGEL) Toggles Elevator locking",
@@ -56,20 +43,17 @@ namespace ATTG3
                 "(VOTEC) Clears Vote",
                 "(VOTES) Displays Vote",
                 "(VOTEBC) Displays how to vote",
-                "-------------EVENT COMMANDS-------------",
-                "(AGCIMTF) Turns on CI VS MTF next round",
                 "-------------OTHER COMMANDS-------------",
                 "(AGHELP) This command",
                 "(AGAMMO) (Players name)",
                 "(AGUP) (Players name)",
-                "(AGCitem) Enables custom items",
-				"(AGDISABLE) Disables Event Plugin",
-				"(AGSHAKE) Shakes all players screens",
+                "(AGDISABLE) Disables Event Plugin",
+                "(AGSHAKE) Shakes all players screens",
                 "----------------------------------------",
                 "Plugin created by All The Time Gaming"
         };
-		}
-	
-		
-	}
+        }
+
+
+    }
 }
