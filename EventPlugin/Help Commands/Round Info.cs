49ec2eb2780bc1a65386b990fc038c41e2a08153
All The Time Gaming <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-04-04 11:03:50 -0500

    New Round Stats Command

---
+++ EventPlugin/Help Commands/Round Info.cs
@@ -0,0 +1,37 @@
+﻿using Smod2.Commands;
+namespace ATTG3
+{
+    class RS : ICommandHandler
+    {
+        private readonly ATTG3Plugin plugin;
+        public RS(ATTG3Plugin plugin)
+        {
+            //Constructor passing plugin refrence to this class
+            this.plugin=plugin;
+        }
+
+        public string GetCommandDescription()
+        {
+            // This prints when someone types HELP HELLO
+            return "";
+        }
+
+        public string GetUsage()
+        {
+            // This prints when someone types HELP HELLO
+            return "AGRS";
+        }
+
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            return new[]
+            {
+                
+                "-------------Round Stats-------------",
+				
+				"----------------------------------------",
+                "Plugin created by All The Time Gaming"
+        };
+        }
+    }
+}
