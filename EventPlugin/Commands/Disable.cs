acfea7e24112f3996b9f55ad92f4685bcee02df2
All The Time Gaming <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-04-10 01:01:55 -0500

    Update Disable.cs

--- EventPlugin/Commands/Disable.cs
+++ EventPlugin/Commands/Disable.cs
@@ -4,44 +4,44 @@ using System.Linq;
 
 namespace ATTG3
 {
-    class Disable : ICommandHandler
-    {
-        private readonly ATTG3Plugin plugin;
-        public Disable(ATTG3Plugin plugin)
-        {
-            //Constructor passing plugin refrence to this class
-            this.plugin = plugin;
-        }
-        public string GetCommandDescription()
-        {
-            // This prints when someone types HELP HELLO
-            return "Disable ATTG Command Plugin";
-        }
-        public string GetUsage()
-        {
-            // This prints when someone types HELP HELLO
-            return "Disable ATTG Command Plugin";
-        }
-        public string[] OnCall(ICommandSender sender, string[] args)
-        {
-            if (!(sender is Server) &&
-                sender is Player player &&
-                !plugin.Disablerank.Contains(player.GetRankName()))
-            {
-                return new[]
-                {
-                    $"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
-                };
-            }
-            else
-            {
-                plugin.Info(sender + " ran the " + GetUsage() + " command!");
-                this.plugin.PluginManager.DisablePlugin(this.plugin);
-            }
-            return new[]
-            {
-                $"ATTG COMMAND PLUGIN DISABLED"
-            };
-        }
-    }
+	class Disable : ICommandHandler
+	{
+		private readonly ATTG3Plugin plugin;
+		public Disable(ATTG3Plugin plugin)
+		{
+			//Constructor passing plugin refrence to this class
+			this.plugin=plugin;
+		}
+		public string GetCommandDescription()
+		{
+			// This prints when someone types HELP HELLO
+			return "Disable ATTG Command Plugin";
+		}
+		public string GetUsage()
+		{
+			// This prints when someone types HELP HELLO
+			return "Disable ATTG Command Plugin";
+		}
+		public string[] OnCall(ICommandSender sender, string[] args)
+		{
+			if (!(sender is Server)&&
+				sender is Player player&&
+				!plugin.Disablerank.Contains(player.GetRankName()))
+			{
+				return new[]
+				{
+					$"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
+				};
+			}
+			else
+			{
+				plugin.Info(sender+" ran the "+GetUsage()+" command!");
+				this.plugin.PluginManager.DisablePlugin(this.plugin);
+			}
+			return new[]
+			{
+				$"ATTG COMMAND PLUGIN DISABLED"
+			};
+		}
+	}
 }
