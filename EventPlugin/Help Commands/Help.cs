1fdea106e39b0e5d623c1fe6279d62b4a44554ad
ALLTHETIMEGAMING <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-05-22 12:28:05 -0500

    Fixed Help Comman

--- EventPlugin/Help Commands/Help.cs
+++ EventPlugin/Help Commands/Help.cs
@@ -6,19 +6,16 @@ namespace ATTG3
         private readonly ATTG3Plugin plugin;
         public Help(ATTG3Plugin plugin)
         {
-            //Constructor passing plugin refrence to this class
             this.plugin=plugin;
         }
 
         public string GetCommandDescription()
         {
-            // This prints when someone types HELP HELLO
             return "Prints Help command";
         }
 
         public string GetUsage()
         {
-            // This prints when someone types HELP HELLO
             return "AGHELP";
         }
 
@@ -27,24 +24,13 @@ namespace ATTG3
             return new[]
             {
                 "ATTG COMMAND PLUGIN COMMANDS",
-                "-------------MAP COMMANDS-------------",
-                "(AGTL) Turns on all Tleslas",
-                "(AGEL) Toggles Elevator locking",
-                "(AGTLR) Toggles Tlesla Gates",
-                "(AGELR) (#) changes Elevator speed",
-				"(AGLIGHT) (#) Turns on / Off all lights in LCZ/HCZ",
-                "-------------SCP COMMANDS-------------",
-                "(AGP079) Opens SCP-079 Generators",
-                "(AGE079) Ejects all tablets from generators",
-                "(AGL079) Levels up 079 and gives AP",
-                "(AG079T) (#) Sets SCP-079 generators startup time",
+                "-------------COMMANDS-------------",
+                "(AGMAP) All Map Commands",
+                "(AGLIGHT) (#) Turns on / Off all lights in LCZ/HCZ",
+                "(AG079) ALL SCP-079 Commands",
                 "(AGSPEED) (Player) (#) Sets 939 player movement speed",
                 "(AGSPEEDA) (#) Sets all 939s speed", 
-				"-------------VOTE COMMANDS-------------",
-                "(AGVOTET) Toggles Voting",
-                "(AGVOTEC) Clears Vote",
-                "(AGVOTES) Displays Vote",
-                "(AGVOTEBC) Displays how to vote",
+                "(AGVOTE) All Voting commands",
 				"-------------HELP COMMANDS-------------",
 				"(AGHELP) This command",
 				"(AGIH) Prints All Items in SCP:SL",
@@ -53,13 +39,10 @@ namespace ATTG3
                 "(AGAMMO) (Players name) Gives player 100,000 Ammo",
                 "(AGLOCK) (Players name) Lets Player Lock Doors",
                 "(AGULOCK) (Players name) Lets Player Unlock Doors",
-                "(AGDISABLE) Disables ATTG Command Plugin",
-                "(AGSHAKE) Shakes all players screens",
-				"(AGLIGHTS) Turns off all lights in HCZ / LCZ",
-				"(AGGEND) Disables all Generators",
                 "(AGSGOD) Starts SCP God Event",
                 "(AGFAKE) (Player Name) Fakes a players death",
                 "(AGWORK) (Player Name) Spawns a Work Station under player",
+                "(AGPLAY) (Player Name) Spawns Unknown",
                 "(AGBLAST) Closes the Nuke blast doors",
                 "----------------------------------------",
                 "Plugin created by All The Time Gaming"
