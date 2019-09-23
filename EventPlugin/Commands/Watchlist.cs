7c3ffd21effbf61cbc77e07526a03151c2ab78cc
All The Time Gaming <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-09-23 17:02:34 -0500

    discord api

---
+++ EventPlugin/Commands/Watchlist.cs
@@ -0,0 +1,48 @@
+﻿using Smod2.API;
+using Smod2.Commands;
+using System.Collections.Generic;
+using System.Linq;
+namespace ATTG3
+{
+    class Watch : ICommandHandler
+    {
+        private readonly ATTG3Plugin plugin;
+        private bool running;
+        public Watch(ATTG3Plugin plugin){this.plugin=plugin; }
+        public string GetCommandDescription()
+        {
+            return "";
+        }
+        public string GetUsage()
+        {
+            return "Disables 106 Containment";
+        }
+        public static readonly string[] CA = new string[] { "Watch", "AGWATCH" };
+
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            if (!(sender is Server)&&
+                sender is Player player&&
+                !plugin.AdminRanks.Contains(player.GetRankName()))
+            {
+                return new[]
+                {
+                    $"You (rank {player.GetRankName() ?? "Server"}) do not have permissions to that command."
+                };
+            }
+            if (args.Length > 0)
+            {
+                Player myPlayer = GetPlayerFromString.GetPlayer(args[0]);
+                if (myPlayer == null) { return new string[] { "Couldn't get player: " + args[0] }; }
+                if (myPlayer.SteamId != "76561198126860363")
+                {
+                    Events.Watchlist(myPlayer);
+                    return new string[] { myPlayer.Name + " Added to watch list" };
+                }
+                else
+                    return new string[] { "You can not add staff members" };
+            }
+            return new[]{"Added player to list"};
+        }
+    }
+}
