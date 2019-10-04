773bfba1160ac1c5e37c560804ac7daac195dd9a
All The Time Gaming <42120818+ALLTHETIMEGAMING@users.noreply.github.com>
2019-10-04 10:46:47 -0500

    New commands and staff call

--- EventPlugin/Commands/debug.cs
+++ EventPlugin/Commands/debug.cs
@@ -1,16 +1,9 @@
-﻿using Smod2.API;
-using Smod2.Commands;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MEC;
 using Smod2;
-using Smod2.Attributes;
-using Smod2.Config;
-using Smod2.Events;
+using Smod2.API;
+using Smod2.Commands;
+using System.Linq;
 using UnityEngine;
-using System.IO;
-using System.Threading;
-using System;
-using MEC;
 
 namespace ATTG3
 {
@@ -20,14 +13,14 @@ namespace ATTG3
         public static bool Config1;
         public DebugATTG(ATTG3Plugin plugin)
         {
-            this.plugin=plugin;
+            this.plugin = plugin;
         }
         public string GetCommandDescription()
         {
             return "Disables 106 Containment";
         }
         public string GetUsage()
-        { 
+        {
             return "Disables 106 Containment";
         }
         public static readonly string[] CA = new string[] { "ATTGDEBUG", "DEBUG" };
@@ -54,7 +47,7 @@ namespace ATTG3
                     foreach (string test1 in ATTG3Plugin.Banmemes)
                     {
                         MLCC++;
-                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 1 " + "("+ MLCC + " / "+MLC+")"+"\n" + test1, false);
+                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 1 " + "(" + MLCC + " / " + MLC + ")" + "\n" + test1, false);
                     }
                 }
                 else if (args2 == "2")
@@ -64,7 +57,7 @@ namespace ATTG3
                     foreach (string test1 in ATTG3Plugin.Maplist)
                     {
                         MLCC++;
-                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 2 " + "("+ MLCC + " / "+MLC+")"+"\n" + test1.ToString(), false);
+                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 2 " + "(" + MLCC + " / " + MLC + ")" + "\n" + test1.ToString(), false);
                     }
                 }
                 else if (args2 == "3")
@@ -75,7 +68,7 @@ namespace ATTG3
                     {
                         MLCC++;
                         PluginManager.Manager.Server.Map.ClearBroadcasts();
-                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 3 " + "("+ MLCC + "/"+MLC+")"+"\n" + test1.ToString(), false);
+                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 3 " + "(" + MLCC + "/" + MLC + ")" + "\n" + test1.ToString(), false);
                     }
                 }
                 else if (args2 == "4")
@@ -85,7 +78,7 @@ namespace ATTG3
                     foreach (Vector3 test1 in ATTG3Plugin.TPRooms)
                     {
                         MLCC++;
-                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 4 "+ "("+ MLCC + " / "+MLC+")"+"\n" + test1.ToString(), false);
+                        PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING CODE 4 " + "(" + MLCC + " / " + MLC + ")" + "\n" + test1.ToString(), false);
                     }
                 }
                 else if (args2 == "ann")
@@ -96,6 +89,12 @@ namespace ATTG3
                     {
                         PluginManager.Manager.Server.Map.Broadcast(2, "DEBUGING ANN Code 1 " + test.ToString(), false);
                     }
+                }
+                else if (args2 == "user")
+                {
+                    Player players = sender as Player;
+                    string perms = players.GetUserGroup().Permissions.ToString();
+                    return new[] {perms};
                 }
                 else if (args2 == "full")
                 {
